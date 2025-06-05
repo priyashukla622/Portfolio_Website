cd218360fd271facccb9be63dbc37a4f1a4d3331
priya shukla <priyashukla22@navgurukul.org>
2025-06-05 18:05:51 +0530

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-500 p-4">
+      Hello Priya, Tailwind v4 is working! 🎉
+    </div>
+  );
+}
+export default App
